6d4305233c961ad32a04b1a988851745b822ef02
Andy Atkinson <andy.atkinson@livingsocial.com>
2013-05-27 18:31:34 -0400

    Remove apt configuration for nginx

--- packages/nginx.rb
+++ packages/nginx.rb
@@ -1,16 +1,3 @@
-package :nginx, :provides => :webserver do
-  description 'Nginx Web Server'
-
-  apt 'nginx' do
-    post :install, '/etc/init.d/nginx start'
-  end
-
-  verify do
-    has_executable '/usr/sbin/nginx'
-    has_executable '/etc/init.d/nginx'
-  end
-end
-
 package :nginx_source, :provides => :webserver do
   requires :build_essential, :nginx_dependencies
   description 'Nginx Web Server'
